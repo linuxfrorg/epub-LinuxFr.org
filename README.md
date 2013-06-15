3caa461c93538d1e7b051f17dffa1377c40b43b8
Bruno Michel <bmichel@menfin.info>
2013-06-15 23:37:03 +0200

    Add the OPF file in epub

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ How to use it?
 
 [Install Go 1](http://golang.org/doc/install) and don't forget to set `$GOPATH`
 
+    # aptitude install libonig-dev libxml2-dev
     $ go get -u github.com/nono/epub-LinuxFr.org
     $ epub-LinuxFr.org [-addr addr] [-l logs]
 
--- epub.go
+++ epub.go
@@ -2,29 +2,33 @@ package main
 
 import (
 	"archive/zip"
+	"bytes"
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/bmizerany/pat"
 	"github.com/moovweb/gokogiri"
-	"github.com/moovweb/gokogiri/html"
+	"github.com/moovweb/gokogiri/css"
 	"github.com/moovweb/gokogiri/xml"
+	"html/template"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"syscall"
+	"time"
 )
 
 type Item struct {
-	Id string
+	Id   string
 	Href string
 	Type string
 }
 
 type Epub struct {
 	Zip *zip.Writer
-	// TODO identifier / language / publisher / rights
+	// TODO identifier / rights
 	Title        string
 	Subject      string
 	Date         string
@@ -44,12 +48,67 @@ const (
 </container>`
 )
 
+var PackageTemplate = template.Must(template.New("package").Parse(
+	`<?xml version="1.0" encoding="utf-8"?>
+<package xmlns="http://www.idpf.org/2007/opf" unique-identifier="bookid" xml:lang="fr">
+	<metadata xmlns:dc="http://purl.org/dc/elements/1.1/">
+		<dc:language id="pub-language">fr</dc:language>
+		<dc:identifier id="pub-identifier">xxx</dc:identifier>
+		{{if .Title}}<dc:title id="pub-title">{{.Title}}</dc:title>{{end}}
+		{{if .Date}}<dc:date>{{.Date}}</dc:date>{{end}}
+		{{if .Creator}}<dc:creator id="pub-creator">{{.Creator}}</dc:creator>{{end}}
+		{{range .Contributors}}<dc:contributor>{{.}}</dc:contributor>{{end}}
+		<dc:rights>xxx</dc:rights>
+	</metadata>
+	<manifest>
+		{{range .Items}}<item id="{{.Id}}" href="{{.Href}}" media-type="{{.Type}}"/>{{end}}
+	</manifest>
+	<spine>
+		{{range .Items}}<itemref idref="{{.Id}}"/>{{end}}
+	</spine>
+</package>`))
+
 func NewEpub(w io.Writer) (epub *Epub) {
 	z := zip.NewWriter(w)
 	epub = &Epub{Zip: z}
 	epub.AddFile("mimetype", ContentType)
 	epub.AddFile("META-INF/container.xml", Container)
 	return
+}
+
+func (epub *Epub) FindMeta(article xml.Node, selector string) string {
+	xpath := css.Convert(selector, css.LOCAL)
+	nodes, err := article.Search(xpath)
+	if err != nil || len(nodes) == 0 {
+		return ""
+	}
+	return nodes[0].Content()
+}
+
+func (epub *Epub) FindMetas(article xml.Node, selector string) []string {
+	xpath := css.Convert(selector, css.LOCAL)
+	nodes, err := article.Search(xpath)
+	if err != nil {
+		return nil
+	}
+	metas := make([]string, len(nodes))
+	for i, node := range nodes {
+		metas[i] = node.Content()
+	}
+	return metas
+}
+
+func (epub *Epub) FillMeta(article xml.Node) {
+	epub.Title = epub.FindMeta(article, "header h1 a:last-child")
+	epub.Subject = epub.FindMeta(article, "header h1 a.topic")
+	meta := epub.FindMeta(article, "header time.updated")
+	// FIXME ParseInLocation
+	date, err := time.Parse("le 02/01/06 à 15:04", meta)
+	if err == nil {
+		epub.Date = date.String()
+	}
+	epub.Creator = epub.FindMeta(article, "header .meta a[rel=\"author\"]")
+	epub.Contributors = epub.FindMetas(article, "header .meta .edited_by a")
 }
 
 func (epub *Epub) AddFile(filename, content string) (err error) {
@@ -69,14 +128,21 @@ func (epub *Epub) AddFile(filename, content string) (err error) {
 }
 
 func (epub *Epub) Close() {
-	epub.AddFile("EPUB/package.opf", "XXX") // FIXME
-	err := epub.Zip.Close()
+	var opf bytes.Buffer
+	err := PackageTemplate.Execute(&opf, epub)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	epub.AddFile("EPUB/package.opf", opf.String())
+	err = epub.Zip.Close()
 	if err != nil {
 		log.Println(err)
 	}
 }
 
-func fetchContent(uri string) (doc *html.HtmlDocument, err error) {
+func fetchArticle(uri string) (article xml.Node, err error) {
 	resp, err := http.Get(uri)
 	if err != nil {
 		return
@@ -89,12 +155,25 @@ func fetchContent(uri string) (doc *html.HtmlDocument, err error) {
 		return
 	}
 
-	doc, err = gokogiri.ParseHtml(body)
+	doc, err := gokogiri.ParseHtml(body)
 	if err != nil {
 		log.Printf("Gokogiri error: %s\n", err)
 		return
 	}
 
+	xpath := css.Convert("#contents article", css.LOCAL)
+	articles, err := doc.Root().Search(xpath)
+	if err != nil {
+		log.Printf("Gokogiri error: %s\n", err)
+		return
+	}
+
+	if len(articles) == 0 {
+		err = errors.New("No article found in the page")
+		return
+	}
+
+	article = articles[0]
 	return
 }
 
@@ -104,16 +183,17 @@ func News(w http.ResponseWriter, r *http.Request) {
 
 	slug := r.URL.Query().Get(":slug")
 	uri := fmt.Sprintf("http://%s/news/%s", Host, slug)
-	doc, err := fetchContent(uri)
+	article, err := fetchArticle(uri)
 	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
 
-	buffer := make([]byte, 4096)
-	buffer, _ = doc.Root().ToHtml([]byte(xml.DefaultEncoding), buffer)
+	// 	buffer := make([]byte, 4096)
+	// 	buffer, _ = doc.Root().ToHtml(xml.DefaultEncodingBytes, buffer)
 
 	epub := NewEpub(w)
+	epub.FillMeta(article)
 	epub.Close()
 }
 
